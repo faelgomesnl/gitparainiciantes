a0781576e8784e8021985817928578f977c62d7d
Rafael Gomes <rafael.gomes@metodotelecom.com.br>
2020-09-23 21:26:22 -0300

    criação index

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.status(200).send({ message: 'Olá mundo, estou funcionando' })
+})
+
+app.get('/ola', (req, res) =>{
+    res.status(200).send({ message: 'Oi eu sou a rota Olá' })
+})
+
+app.listen(4001, () =>{
+    console.log('Api rodando na porta 3001');
+})
